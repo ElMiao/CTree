90380c7201e7c74d753dea13f519636b19be990d
elmiao <leonardocampoverdeprof@gmail.com>
2023-11-07 13:12:12 +0100

    made a header file

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include "tree.h"
 
 
 void showHelp(){
@@ -17,66 +17,7 @@ void showHelp(){
 
 }
 
-struct tree{
-	struct tree* parent;
-	struct tree* branches;
-	int nBranches;
-	int dept;
-	int value;
 
-};
-
-
-
-
-struct tree* makeTree(int value){
-
-	
-	struct tree* t=(struct tree*) malloc(sizeof(struct tree));
-	t->parent=NULL;
-	t->dept=0;
-	t->value=0;
-	t->branches=NULL;
-	t->nBranches=0;
-	return t;
-
-}
-
-
-
-void showTreeInfo(struct tree* t){
-
-	printf("TREE INFO:\nDEPT: %d\nVALUE: %d\n",t->dept,t->value);
-	printf("NUMBER OF BRANCHES: %d\n",t->nBranches);
-
-}
-
-void changeValue(struct tree* t,int value){
-
-
-	t->value=value;
-
-}
-
-void moveToTree(struct tree** tr,int direction){
-	
-	struct tree* t= *tr;
-
-	if(direction==0&& t->parent!=NULL){
-		*tr=t->parent;
-	}
-	else if (direction <  t->nBranches){
-
-		*tr=&t->branches[direction-1];
-
-	}
-	else{
-		printf("Direction not allowed: %d\n",direction);
-		return;
-	}
-	printf("Moved successfully\n");
-
-}
 
 int getDirection(){
 
@@ -89,32 +30,7 @@ int getDirection(){
 
 }
 
-void addTree(struct tree* t){
-	
-	if(t->nBranches==0){//if the tree hasn't branches
-		
-		t->branches=(struct tree*)malloc(sizeof(*t));
-		t->nBranches=1;
-		
 
-
-	}else{
-		t->nBranches++;
-		t->branches=realloc(t->branches,sizeof(*t)*t->nBranches);
-
-
-	}
-	struct tree* newTree; newTree=&t->branches[t->nBranches-1]; 
-	newTree->parent=t;
-	
-	newTree->value=0;
-	
-	newTree->branches=NULL;
-	newTree->nBranches=0;
-	newTree->dept=t->dept+1;
-	
-
-}
 
 int main(){
 	
@@ -123,7 +39,7 @@ int main(){
 	char op='h';
 
 
-	printf("TREE MENU:\nPress h for help.");
+	printf("TREE MENU:\nPress h for help.\n");
 	while(op!='q'){
 		printf("ENTER OPTION: ");
 		scanf(" %c",&op);
---
+++ tree.h
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+void treeHelp(){
+
+
+	printf("ctree.h: dinamyc tree c library\n"
+	"This library works with alloc and realloc memory management to make dinamyc trees with more than 2 branches\n"
+	"Each tree has a dept, value (int), the parent tree wich will be null for the initial tree and an array of branches that the current tree has\n"
+	"\n\nFunctions:\n"
+	"\"makeTree()\" -> struct tree*: will return a pointer to a tree with \"dept\":0 and \"value\": 0\n"
+	"\"showTreeInfo(struct tree*)\" -> void: shows to the standard input the value,dept and number of branches of the current tree"
+	"\"changeValue(struct tree*,int)\" -> void: change the value of the current tree\n"
+	"\"moveToTree(struc tree**,int)\" -> void: move to the desired tree based on the direction given as an integer number: 0 to go to the parent, 1,2,3... to go to the desired branch (branch 1, branch 2, branch 3...)\n"
+	"\"addTree(struct tree*)\" -> void: add a child tree to the current tree and automatically set the dept as the dept of the current tree + 1, the value is setted to 0\n"
+	);
+
+	
+
+
+
+}
+
+
+struct tree{
+	struct tree* parent;
+	struct tree* branches;
+	int nBranches;
+	int dept;
+	int value;
+
+};
+
+
+
+
+struct tree* makeTree(int value){
+
+	
+	struct tree* t=(struct tree*) malloc(sizeof(struct tree));
+	t->parent=NULL;
+	t->dept=0;
+	t->value=0;
+	t->branches=NULL;
+	t->nBranches=0;
+	return t;
+
+}
+
+
+
+void showTreeInfo(struct tree* t){
+
+	printf("TREE INFO:\nDEPT: %d\nVALUE: %d\n",t->dept,t->value);
+	printf("NUMBER OF BRANCHES: %d\n",t->nBranches);
+
+}
+
+void changeValue(struct tree* t,int value){
+
+
+	t->value=value;
+
+}
+
+void moveToTree(struct tree** tr,int direction){
+	
+	struct tree* t= *tr;
+
+	if(direction==0&& t->parent!=NULL){
+		*tr=t->parent;
+	}
+	else if (direction <  t->nBranches){
+
+		*tr=&t->branches[direction-1];
+
+	}
+	else{
+		printf("Direction not allowed: %d\n",direction);
+		return;
+	}
+
+}
+
+
+void addTree(struct tree* t){
+	
+	if(t->nBranches==0){//if the tree hasn't branches
+		
+		t->branches=(struct tree*)malloc(sizeof(*t));
+		t->nBranches=1;
+		
+
+
+	}else{
+		t->nBranches++;
+		t->branches=realloc(t->branches,sizeof(*t)*t->nBranches);
+
+
+	}
+	struct tree* newTree; newTree=&t->branches[t->nBranches-1]; 
+	newTree->parent=t;
+	
+	newTree->value=0;
+	
+	newTree->branches=NULL;
+	newTree->nBranches=0;
+	newTree->dept=t->dept+1;
+	
+
+}
